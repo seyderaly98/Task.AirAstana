892a23b42ff67f97b764fb26572dd383d6551e07
Almat mc air m1 <seyderaly98@gmail.com>
2025-10-19 21:46:10 +0500

    Добавил endpoint "Авторизация пользователя"

--- src/Task.AirAstana.API.Presentation/Controllers/AuthController.cs
+++ src/Task.AirAstana.API.Presentation/Controllers/AuthController.cs
@@ -1,4 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using Task.AirAstana.Application.Features.Auth.Commands;
+using Task.AirAstana.Infrastructure.Identity;
 
 namespace Task.AirAstana.API.Presentation.Controllers;
 
@@ -6,15 +11,27 @@ namespace Task.AirAstana.API.Presentation.Controllers;
 [Route("api/[controller]")]
 public class AuthController : ControllerBase
 {
+    private readonly IMediator _mediator;
 
+    public AuthController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
 
     /// <summary>
     /// Авторизация пользователя
     /// </summary>
+    /// <param name="command">Данные для входа (Username, Password)</param>
+    /// <returns>JWT токен и информация о пользователе</returns>
+    /// <response code="200">Успешная авторизация</response>
+    /// <response code="400">Неверные учетные данные</response>
     [HttpPost("Login")]
-    public async Task<IActionResult> Login()
+    [ProducesResponseType(typeof(Task.AirAstana.Application.Features.Auth.Models.AuthResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Login([FromBody] LoginCommand command)
     {
-        return Ok();
+        var result = await _mediator.Send(command);
+        return Ok(result);
     }
         
 }
