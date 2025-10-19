ac04bba37edfaaa26a73c184ab9309a093e7c873
Almat mc air m1 <seyderaly98@gmail.com>
2025-10-19 21:10:43 +0500

    Добавление DependencyInjection для Application

---
+++ src/Task.AirAstana.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Task.AirAstana.Application.Common.Behaviours;
+
+namespace Task.AirAstana.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        });
+        return services;
+    }
+}
